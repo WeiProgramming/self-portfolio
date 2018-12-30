2322a53a6f0a2d6df27aedaf17f7db6c1e2e35ad
Wei Leung <wei.leung1995@gmail.com>
2018-12-30 02:49:29 -0800

    added in route animations probably move back to sidenav

--- src/app/public/layouts/default-layout/default-layout.component.ts
+++ src/app/public/layouts/default-layout/default-layout.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { trigger, query, group, style, transition} from '@angular/animations';
+import {trigger, query, group, style, transition, animate} from '@angular/animations';
 import {ActivatedRoute} from '@angular/router';
 
 @Component({
@@ -7,7 +7,26 @@ import {ActivatedRoute} from '@angular/router';
   templateUrl: './default-layout.component.html',
   styleUrls: ['./default-layout.component.scss'],
   animations: [
-    trigger('routeAnimate', [])
+    trigger('routeAnimate', [
+      transition('home => companies, companies => contact, contact => resume', [
+        style({height: '!'}),
+        query(':enter', style({transform: 'translateX(100%)', opacity: '0'})),
+        query(':enter , :leave', style({position: 'absolute', left: 0, right: 0, top: 0})),
+        group([
+          query(':enter', animate('.5s ease-out', style({transform: 'translateX(0)', opacity: '1'}))),
+          query(':leave', animate('.5s ease-out', style({transform: 'translateX(-100%)', opacity: '0'})))
+        ])
+      ]),
+      transition('companies => home, contact => companies, resume => contact', [
+        style({height: '!'}),
+        query(':enter', style({transform: 'translateX(-100%)', opacity: '0'})),
+        query(':enter , :leave', style({position: 'absolute', left: '0', right: '0', top: '0', bottom: '0'})),
+        group([
+          query(':enter', animate('.5s ease-out', style({transform: 'translateX(0)', opacity: '1'}))),
+          query(':leave', animate('.5s ease-out', style({transform: 'translateX(100%)', opacity: '0'})))
+        ])
+      ])
+    ])
   ]
 })
 export class DefaultLayoutComponent implements OnInit {
@@ -16,7 +35,7 @@ export class DefaultLayoutComponent implements OnInit {
 
   ngOnInit() {
   }
-  getData(outlet): string{
+  getData(outlet): string {
     return outlet.activatedRoute.data.value['pageName'];
   }
 
